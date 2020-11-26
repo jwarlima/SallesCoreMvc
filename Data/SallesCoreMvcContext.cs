652b21e7a4cee16d24e3899f360f3dd718dc5e45
Warley Lima <jwar.lima@gmail.com>
2020-11-26 16:09:13 -0300

    Recriado o controller e view de Department usando scaffolding

---
+++ Data/SallesCoreMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallesCoreMvc.Models;
+
+namespace SallesCoreMvc.Data
+{
+    public class SallesCoreMvcContext : DbContext
+    {
+        public SallesCoreMvcContext (DbContextOptions<SallesCoreMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SallesCoreMvc.Models.Department> Department { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallesCoreMvc.Data;
 
 namespace SallesCoreMvc
 {
@@ -33,6 +35,9 @@ namespace SallesCoreMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SallesCoreMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SallesCoreMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
