8b2622a6508fb3467442b76c079b6ea4b34bb12a
Warley Lima <jwar.lima@gmail.com>
2020-11-26 17:38:31 -0300

    Adicionado outras entidades ao projeto e a segunda Migration

--- Data/SallesCoreMvcContext.cs
+++ Data/SallesCoreMvcContext.cs
@@ -14,6 +14,8 @@ namespace SallesCoreMvc.Data
         {
         }
 
-        public DbSet<SallesCoreMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
