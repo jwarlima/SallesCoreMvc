0d78c8bde38dee64e1030f7df989b95c7d99c480
Warley Lima <jwar.lima@gmail.com>
2020-11-27 10:57:16 -0300

    Criação do SellerFormViewModel e Department para seleção de componente

---
+++ Services/DepartmentService.cs
@@ -0,0 +1,25 @@
+﻿using SallesCoreMvc.Data;
+using SallesCoreMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SallesCoreMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SallesCoreMvcContext _context;
+
+        public DepartmentService(SallesCoreMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            //retorna os departamentos ordenando por nome e convertido em lista ao final.
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
